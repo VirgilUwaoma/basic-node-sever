ee4c78400a46c0afd9c63eba1d63421173d6dcf3
Virgil <virgiluwaoma@gmail.com>
2022-08-08 22:49:04 +0100

    feat: Create basic node server

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 8080;
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
